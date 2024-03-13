1ca7140c669dc86ed31d0d8616870215736edad6
Emmanuel Kemakolam <emmakema47@gmail.com>
2024-03-13 09:59:32 +0100

    First commit

---
+++ src/test/java/com/emmakema47/demoapp/DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emmakema47.demoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
