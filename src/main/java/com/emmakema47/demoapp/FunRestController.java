7583f579d220c1936e0424595c4117d980e3d8bf
Emmanuel Kemakolam <emmakema47@gmail.com>
2024-03-14 10:30:31 +0100

    Added Spring Boot Properties in the application

--- src/main/java/com/emmakema47/demoapp/FunRestController.java
+++ src/main/java/com/emmakema47/demoapp/FunRestController.java
@@ -1,10 +1,19 @@
 package com.emmakema47.demoapp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    // Inject properties for: coach.name and team.name
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
 
     // expose "/" that returns "Hello World"
 
